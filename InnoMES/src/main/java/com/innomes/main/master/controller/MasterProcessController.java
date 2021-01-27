db63d2d7092d6b37319cdd84c499eed3102b3c14
SeoYoungEui <tjduddml@hanmail.net>
2021-01-27 20:31:03 +0900

    공정정보

--- InnoMES/src/main/java/com/innomes/main/master/controller/MasterProcessController.java
+++ InnoMES/src/main/java/com/innomes/main/master/controller/MasterProcessController.java
@@ -1,5 +1,9 @@
 package com.innomes.main.master.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.innomes.main.exception.CProcessSaveException;
 import com.innomes.main.master.dto.MasterProcessDTO;
 import com.innomes.main.master.param.MasterProcessParam;
 import com.innomes.main.master.service.MasterProcessService;
+import com.innomes.main.response.model.CommonResult;
 import com.innomes.main.response.model.PageListResult;
 import com.innomes.main.response.service.ResponseService;
 
@@ -28,14 +34,30 @@ public class MasterProcessController {
 	private ResponseService responseService;
 	@Autowired
 	private MasterProcessService masterProcessService;
-	
+
 	@ApiOperation(value = "공정 정보 조회", notes = "공정 정보를 반환합니다. (검색조건 필터링 가능)")
 	@CrossOrigin
 	@PostMapping("/master/processList")
-	public PageListResult<MasterProcessDTO> getMaterProcessList(@RequestBody(required = false) MasterProcessParam masterProcessParam ,final Pageable pageable){
-		
-		if (masterProcessParam == null) masterProcessParam = new MasterProcessParam(); // 전체 조회
-		
+	public PageListResult<MasterProcessDTO> getMaterProcessList(@RequestBody(required = false) MasterProcessParam masterProcessParam, final Pageable pageable) {
+
+		if (masterProcessParam == null)
+			masterProcessParam = new MasterProcessParam(); // 전체 조회
+
 		return responseService.getPageListResult(MasterProcessDTO.class, masterProcessService.getMaterProcessList(masterProcessParam, pageable));
+	}
+
+	//공정정보 insert & update
+	@ApiOperation(value = "공정 정보 저장", notes = "공정 정보를 저장합니다.")
+	@CrossOrigin
+	@PostMapping("/master/processInfo/save")
+	public CommonResult saveMasterProcess(@RequestBody(required = true) @Valid List<MasterProcessParam> paramList) {
+
+		boolean result = masterProcessService.saveMasterProcess(paramList);
+
+		if (result == true) {
+			return responseService.getSuccessResult();
+		} else {
+			throw new CProcessSaveException();
+		}
 	}
 }
--- InnoMES/src/main/java/com/innomes/main/master/param/MasterProcessParam.java
+++ InnoMES/src/main/java/com/innomes/main/master/param/MasterProcessParam.java
@@ -1,90 +1,20 @@
 package com.innomes.main.master.param;
 
-import java.util.Date;
+import com.innomes.main.master.model.MST120;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class MasterProcessParam {
-	@ApiModelProperty(required = true, value = "공정코드(PK)"
-			+ "\n ProcessCode"
-			+ "\n MST120.PROC_CODE")
-	private String procCode;
-	
-	@ApiModelProperty(required = true, value = "공정명"
-			+ "\n Process Name"
-			+ "\n MST120.PROC_NAME")	
-	private String procName;
-
-	@ApiModelProperty(value = "공정유형"
-			+ "\n Process type"
-			+ "\n MST120.PROC_TYPE")
-	private String procType;
-	
-	@ApiModelProperty(value = "내외작구분 (0: 사내 / 1: 외작 / 2: 내외작)"
-			+ "\n Insourcing/Outsourcing(0 : Insourcing, shipping(IN, SP) / 1 : Outsourcing / 2 : Both)"
-			+ "\n MST120.IN_OUT_TYPE")
-	private int inOutType;
-	
-	@ApiModelProperty(value = "생산공정유무 (0: 생산공정 / 1: 비생산공정)"
-			+ "\n Production Process?(0 : Prod.Process / 1 : Non-prod. process(IN, SP))"
-			+ "\n MST120.PRDTION_YN")
-	private int prdtionYN;
-	
-	@ApiModelProperty(value = "수불관리유무(0: 수불관리 / 1: 비수불관리)"
-			+ "\n IN-OUT management(0 : Manage / 1 : Not manage)"
-			+ "\n MST120.SUPLLY_YN")
-	private int supplyYN;
-	
-	@ApiModelProperty(value = "지시유무 (0: 지시공정 / 1: 비지시공정)"
-			+ "\n Ordered?(0 : Process with order / 1 : Process without order)"
-			+ "\n MST120.WORK_ORDER_YN")
-	private int workOrderYN;
-	
-	@ApiModelProperty(value = "기본공정여부 (0: 필수 / 1: 사용자 지정)"
-			+ "\n Default Process(0 : Default(IN, SP) / 1 : User assignment)"
-			+ "\n MST120.DEFAULT_YN")
+	/* search */
 	private Integer defaultYN;
 	
-	@ApiModelProperty(required = true, value = "최종검사 여부 (0: Y / 1: N)"
-			+ "\n FinalInspection?"
-			+ "\n MST120.PROC_NAME")
-	private Integer inspFinishedYN;
+	private String procCode;
 	
-	@ApiModelProperty(required = true, value = "조립공정 여부 (0: Y / 1: N)"
-			+ "\n Assembly Process?"
-			+ "\n MST120.AS_YN")
-	private Integer asYN;
+	private String procName;
 	
-	@ApiModelProperty(value = "비고"
-			+ "\n Remarks"
-			+ "\n MST120.DESCRIPTION")
-	private String description;
-	
-	@ApiModelProperty(required = true, value = "생성자"
-			+ "\n Author"
-			+ "\n MST120.CREATE_USER")
-	private String createUser;
-
-	@ApiModelProperty(required = true, value = "생성일시"
-			+ "\n Created time"
-			+ "\n MST120.CREATE_TIME")
-	private Date createTime;
-	
-	@ApiModelProperty(value = "수정자"
-			+ "\n Modified by"
-			+ "\n MST120.UPDATE_USER")
-	private String updateUser;
-	
-	@ApiModelProperty(value = "수정일시"
-			+ "\n Modified time"
-			+ "\n MST120.UPDATE_TIME")
-	private Date updateTime;
-	
-	@ApiModelProperty(required = true, value = "사용여부 (0: 가용 / 1: 비가용)"
-			+ "\n Use/Unuse(0 : Use / 1 : Unuse)"
-			+ "\n MST120.USED")
-	private int used = 0;
+	private String procType;
+	/* save */
+	private MST120 mst120;
 }
