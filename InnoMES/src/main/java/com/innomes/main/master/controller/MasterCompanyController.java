4dc7c0b12e333ec29082ef9cf1825c79c83ad063
ba91 <ba91@e3view.com>
2021-01-27 20:12:51 +0900

    거래처 저장

--- InnoMES/src/main/java/com/innomes/main/master/controller/MasterCompanyController.java
+++ InnoMES/src/main/java/com/innomes/main/master/controller/MasterCompanyController.java
@@ -1,5 +1,7 @@
 package com.innomes.main.master.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.innomes.main.exception.CCompanySaveException;
 import com.innomes.main.master.dto.MasterCompanyDTO;
+import com.innomes.main.master.model.MST150;
 import com.innomes.main.master.param.MasterCompanyParam;
 import com.innomes.main.master.service.MasterCompanyService;
+import com.innomes.main.response.model.CommonResult;
 import com.innomes.main.response.model.PageListResult;
 import com.innomes.main.response.service.ResponseService;
 
@@ -37,5 +42,18 @@ public class MasterCompanyController {
 			masterCompanyParam = new MasterCompanyParam();
 		
 		return responseService.getPageListResult(MasterCompanyDTO.class, masterCompanyService.getCompInfo(masterCompanyParam, pageable));
+	}
+	
+	@CrossOrigin
+	@ApiOperation(value = "거래처 정보 저장", notes = "거래처 정보를 저장합니다.")
+	@PostMapping("/master/company/save")
+	public CommonResult setCompInfo(@RequestBody(required = true) List<MST150> compParam) {
+		boolean result = masterCompanyService.setCompInfo(compParam);
+		
+		if(result) {
+			return responseService.getSuccessResult();
+		}else {
+			throw new CCompanySaveException();
+		}
 	}
 }
--- InnoMES/src/main/java/com/innomes/main/master/service/MasterCompanyService.java
+++ InnoMES/src/main/java/com/innomes/main/master/service/MasterCompanyService.java
@@ -1,15 +1,18 @@
 package com.innomes.main.master.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 import com.google.common.reflect.TypeToken;
 import com.innomes.main.master.dto.MasterCompanyDTO;
@@ -31,5 +34,23 @@ public class MasterCompanyService {
 		//modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
 		Page<MST150> output = mst150Repository.findAllLike(masterCompanyParam, pageable);
 		return new PageImpl<>(modelMapper.map(output.getContent(), new TypeToken<List<MasterCompanyDTO>>(){}.getType()),pageable, output.getTotalElements());
+	}
+	public boolean setCompInfo(List<MST150> compParam) {
+		boolean success = true;
+		
+		try {
+			for (MST150 mst150 : compParam) {
+				mst150.setCreateTime(new Date());
+				mst150.setUpdateTime(new Date());
+			}
+			
+			mst150Repository.saveAll(compParam);
+			mst150Repository.flush();
+		}catch(Exception e) {
+			e.printStackTrace();
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+		}
+		
+		return success;
 	}
 }
