90168ff3d4ad62db8369575b99c3c36e0eea6bf3
SeoYoungEui <tjduddml@hanmail.net>
2021-02-08 20:36:02 +0900

    공구정보관리 저장

--- InnoMES/src/main/java/com/innomes/main/master/param/MasterToolParam.java
+++ InnoMES/src/main/java/com/innomes/main/master/param/MasterToolParam.java
@@ -1,8 +1,13 @@
 package com.innomes.main.master.param;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.innomes.main.master.model.MST110;
 import com.innomes.main.master.model.MST113;
 
@@ -25,6 +30,8 @@ public class MasterToolParam implements Pageable{
 
 	/* search */
 	// MST110 PARAM
+	private String itemId;
+	
 	private String itemCode;
 	
 	private String itemName;
@@ -33,12 +40,52 @@ public class MasterToolParam implements Pageable{
 	
 	private int invType;
 	
+	private double lotSize;
+
+	private String lotUnit;
+
+	private double safetyQnt;
+
+	private String safetyUnit;
+
+	private String locCode;
+
+	private String description;
+	
+	private int used;
+	
+	private String createUser;
+	
+	private Date createTime;
+	
+	private String updateUser;
+	
+	private Date updateTime;
+	
 	// MST113 PARAM
 	private String toolType; // 공구유형
 	
 	private String toolCtg; // 공구카테고리
 	
 	private String toolGroup; // 공구 그룹
+	
+	private String toolId;
+	
+	private String toolProc;
+	
+	private String prdtionProc;
+	
+	private double toolLifeCnt;
+	
+	private double toolWarningRate;
+	
+	private double toolChkCycle;
+	
+	private double toolRecycleCnt;
+	
+	private int lotYN;
+	
+	private String repItemId;
 
 	@Override
 	public String toString() {
