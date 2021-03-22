8ef6d3d8cd71f762684e737b39fd41158fcf9ff4
SeoYoungEui <tjduddml@hanmail.net>
2021-03-22 09:47:05 +0900

    no message

--- InnoMES/src/main/java/com/innomes/main/code/controller/StopCodeController.java
+++ InnoMES/src/main/java/com/innomes/main/code/controller/StopCodeController.java
@@ -22,44 +22,47 @@ import com.innomes.main.response.model.CommonResult;
 import com.innomes.main.response.model.PageListResult;
 import com.innomes.main.response.service.ResponseService;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Api(value = "StopCodeController")
 public class StopCodeController {
-	
+
 	@Autowired
 	private ResponseService responseService;
-	
+
 	@Autowired
 	private StopCodeService stopCodeService;
-	
+
 	@Autowired
 	private CodePoolService codePoolService;
-	
+
 	@ApiOperation(value = "비가동유형 전체 조회", notes = "코드정보 전체를 반환합니다. (검색조건 필터링 가능)")
 	@CrossOrigin
 	@PostMapping("/master/stopCode")
-	public PageListResult<COD300> findAll(@RequestBody(required = false) COD300Param cod300Param,
-			@PageableDefault(value = 1000) final Pageable pageable) {
-		if(cod300Param == null) //파라메터가 없을경우
+	public PageListResult<COD300> findAll(@RequestBody(required = false) COD300Param cod300Param, @PageableDefault(value = 1000) final Pageable pageable) {
+		if (cod300Param == null) //파라메터가 없을경우
 			cod300Param = new COD300Param(); // 전체 조회
-		
+
 		return responseService.getPageListResult(COD300.class, stopCodeService.findAllLike(cod300Param, pageable));
 	}
+
 	//insert & update
-		@CrossOrigin
-		@PostMapping("/master/stopCode/save")
-		public CommonResult saveCodeInfo(@Valid @RequestBody(required = true) List<COD300Param> cod300ParamList) {
-			boolean result = stopCodeService.saveStopCode(cod300ParamList);
-			
-			if(result == true) {
-				codePoolService.SetCOD300List();
-				return responseService.getSuccessResult();
-			} else {
-				throw new CStopCodeInfoSaveException();
-			}
+	@ApiOperation(value = "비가동유형 정보 저장", notes = "비가동유형정보를 저장합니다.")
+	@CrossOrigin
+	@PostMapping("/master/stopCode/save")
+	public CommonResult saveCodeInfo(@Valid @RequestBody(required = true) List<COD300Param> cod300ParamList) {
+		boolean result = stopCodeService.saveStopCode(cod300ParamList);
+
+		if (result == true) {
+			codePoolService.SetCOD300List();
+			return responseService.getSuccessResult();
+		} else {
+			throw new CStopCodeInfoSaveException();
 		}
+	}
 }
